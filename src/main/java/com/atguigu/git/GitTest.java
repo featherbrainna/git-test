9c66ccfc3e60779e43db4d0d2c9b435f474a2ceb
Wangzhongyi <578996401@qq.com>
2022-05-08 14:58:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 王忠义
+ * @version 1.0
+ * @date: 2022/5/8 14:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
